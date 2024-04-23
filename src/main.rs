3c517af25d5aeee917058513fbc0efbc5e3a7813
himanushi <himanushi@users.noreply.github.com>
2024-04-23 19:51:16 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use std::thread;
+use std::time::Duration;
 
-    // Bind the log crate to the ESP Logging facilities
+fn main() {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("hey!!!");
+    log::info!("start!!!");
+
+    loop {
+        thread::sleep(Duration::from_millis(1000));
+        log::info!("hey!!!");
+    }
 }
