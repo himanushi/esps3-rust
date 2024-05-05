ef632bd704cc27cc3f024e4495df37c0a6ddc874
himanushi <himanushi@users.noreply.github.com>
2024-05-05 16:15:16 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ fn main() {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("スタート!!!");
+    log::info!("start!!!");
 
     loop {
         thread::sleep(Duration::from_millis(5000));
-        log::info!("time!!!");
+        log::info!("hey!!!");
     }
 }
